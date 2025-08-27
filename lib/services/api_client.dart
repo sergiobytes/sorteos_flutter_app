4b5996be8bd8801846b1d9a4b418afd333e04c70
Sergio Barreras <sergiobg.isc@gmail.com>
2025-08-27 07:09:31 -0700

    Add permissions for internet and camera in AndroidManifest.xml; implement ApiClient for API interactions

---
+++ lib/services/api_client.dart
@@ -0,0 +1,111 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
+import 'package:sorteos_app/config/constants/environment.dart';
+
+class ApiClient {
+  static String apiBase = Environment.apiUrl;
+  final http.Client _http = http.Client();
+  final Dio _dio = Dio();
+
+  Future<Map<String, dynamic>> getUploadSignature() async {
+    final url = Uri.parse('$apiBase/sign-upload');
+    final resp = await _http.post(url);
+
+    if (resp.statusCode != 200) {
+      throw Exception('Error firmando upload');
+    }
+
+    return jsonDecode(resp.body) as Map<String, dynamic>;
+  }
+
+  Future<Map<String, dynamic>> uploadToCloudinary({
+    required Map<String, dynamic> signPayload,
+    required File imageFile,
+  }) async {
+    final cloudName = signPayload['cloudName'];
+    final apiKey = signPayload['apiKey'].toString();
+    final timestamp = signPayload['timestamp'].toString();
+    final signature = signPayload['signature'].toString();
+    final folder = signPayload['folder'].toString();
+    final type = signPayload['type'].toString();
+
+    final formData = FormData.fromMap({
+      'file': await MultipartFile.fromFile(imageFile.path),
+      'api_key': apiKey,
+      'timestamp': timestamp,
+      'signature': signature,
+      'folder': folder,
+      'type': type,
+    });
+
+    final url = 'http://api.cloudinary.com/v1_1/$cloudName/image/upload';
+    final resp = await _dio.post(url, data: formData);
+
+    if (resp.statusCode != 200) {
+      throw Exception('Fallo al subir a Cloudinary');
+    }
+
+    final data = resp.data as Map<String, dynamic>;
+
+    return {
+      'public_id': data['public_id'],
+      'version': data['version']?.toString(),
+    };
+  }
+
+  Future<void> createParticipant({
+    required String name,
+    required String walletNumber,
+    required String phone,
+    required String photoPublicId,
+    String? photoVersion,
+  }) async {
+    final url = Uri.parse('$apiBase/participants');
+
+    final body = jsonEncode({
+      'name': name,
+      'walletNumber': walletNumber,
+      'phone': phone,
+      'photoPublicId': photoPublicId,
+      'photoVersion': photoVersion,
+    });
+
+    final resp = await _http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: body,
+    );
+
+    if (resp.statusCode != 201) {
+      throw Exception('Error creando participante');
+    }
+  }
+
+  Future<File> downloadZip({
+    required String idToken,
+    required String savePath,
+  }) async {
+    final url = '$apiBase/admin/export';
+
+    final resp = await _dio.get(
+      url,
+      options: Options(
+        headers: {'Authorization': 'Bearer $idToken'},
+        responseType: ResponseType.stream,
+      ),
+    );
+
+    if (resp.statusCode != 200) {
+      throw Exception('Error descargando archivo ZIP');
+    }
+
+    final file = File(savePath);
+    final sink = file.openWrite();
+    await resp.data.stream.pipe(sink);
+    await sink.close();
+
+    return file;
+  }
+}
