ded1ab86e9bb82341a2c62a4649cfc5eb2831386
Sergio Barreras <sergiobg.isc@gmail.com>
2025-08-31 12:39:29 -0600

    Remove unused participant-related classes and services to streamline the codebase

--- lib/domain/repositories/participant_repository.dart
+++
@@ -1,8 +0,0 @@
-import '../entities/participant.dart';
-
-abstract class ParticipantRepository {
-  Future<void> register(Participant participant);
-  Future<List<Participant>> list();
-  Future<void> purge();
-  Future<void> export();
-}
